7bae09d1a885c0976d48adfe5566b9ce221f328b
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2023-12-27 18:14:34 -0300

    Update codesLCD.s

--- codesLCD.s
+++ codesLCD.s
@@ -1,101 +1,179 @@
-.include "memManagement.s"
-.macro putLCDPinsOutput
-    GPIOSetDirection output, pin_RS
-    GPIOSetDirection output, pin_E
-    GPIOSetDirection output, pin_D4
-    GPIOSetDirection output, pin_D5
-    GPIOSetDirection output, pin_D6
-    GPIOSetDirection output, pin_D7
-.endm
 
-@r12 é para sinalizar se é pra incialização, usando somente 4 bits, ou normal - 8 bits (1 inicialização, 0 - transmissão normal)
-@ mov r10, 9 bits @r10 tem os bits de transmissao pro LCD
-@ r9 faz a máscara pra pegar bit a bit
-@ r8 é contador de 4 bits
-@ r11 é para registrar o endereço base dos pinos (pin_rs)
-instructionCode:
-    ldr r11, =pin_RS
-    and r9, r10, #0b100000000 @Mascara para pegar o mais significativo
-    mvn r9, r9
-    GPIOSet r11, r9 @Setagem do Pino RS
-    lsr r10, #1
-    mov r8, #1 @r8 é contador
-    add r11, 0x10 @Vai do pin_RS para o pin_D4 no data
-    
-firstNibble:
-    and r9, r10, #0b100000000 @Mascara para pegar o mais significativo
-    GPIOSet r11, r9 @Setagem do Pino 
-    lsr r10, #1
-    addi r8, r8, #1 @incremento o contador
-    cmp r8, #4 @comparo com 4 o contador
-    add r11, 0x10 @Vai de um pino para o próximo
-    ble firstNibble
-    enable
-    sub r11, 0x40 @restaura o valor do endereço para o pin_D4
-    cmp r12, #0
-    beq lastNibble
-    bx lr
-    
-lastNibble:
-    and r9, r10, #1 @Mascara para pegar o menos significativo
-    GPIOSet pin_D4, r9 @Setagem do Pino 
-    lsr r10, #1
-    add r8, r8, #1 @incremento o contador
-    cmp r8, #4 @comparo com 4 o contador
-    add r11, 0x10 @Vai de um pino para o próximo
-    ble lastNibble
-    enable
-    bx lr
 
-.macro enable
-    nanoSleep totalLoadingTimeRs
-    GPIOSet pin_E, high
-    nanoSleep totalEnableTimeHigh
-    GPIOSet pin_E, low
-    nanoSleep remainingEnableTime
+.macro setLcdPins
+    ldr r4, =pin_E
+    mov r5, #output
+    bl setDirectionGPIO
+
+    ldr r4, =pin_RS
+    mov r5, #output
+    bl setDirectionGPIO
+
+    ldr r4, =pin_D7
+    mov r5, #output
+    bl setDirectionGPIO
+
+    ldr r4, =pin_D6
+    mov r5, #output
+    bl setDirectionGPIO
+
+    ldr r4, =pin_D5
+    mov r5, #output
+    bl setDirectionGPIO
+
+    ldr r4, =pin_D4
+    mov r5, #output
+    bl setDirectionGPIO
 .endm
 
 .macro setSecondLine
-    mov r10, #0b111000000
+    mov r2, #0b111000000
+    mov r3, #0
     bl instructionCode
 .endm
 
 .macro returnHome
-    mov r10, #0b100000010
+    mov r2, #0b100000010
+    mov r3, #0
+    bl instructionCode
+.endm
+
+.macro shiftCursorRight
+    mov r2, #0b100010100
+    mov r3, #0
+    bl instructionCode
+.endm
+
+.macro clearDisplay
+    mov r2, #0b100000001
+    mov r3, #0
     bl instructionCode
 .endm
 
 .macro initialize
 
-    nanoSleep zeMeMama
-    mov r10, #0b10011
-    mov r12, #1
+    nanoSleep timerLcd1
+    @Primeira parte da inicialização
+    mov r2, #0b100110000
+    mov r3, #0
+    bl instructionCode
+    nanoSleep timerLcd2
+
+    @Segunda Parte da inicialização
+    mov r2, #0b100110000
+    mov r3, #1
+    bl instructionCode
+    nanoSleep timerLcd3
+
+    @Terceira parte da inicialização
+    mov r2, #0b100110000
+    mov r3, #1
     bl instructionCode
     
-    nanoSleep zeMeMama2
+    @Colocando como 4 bits
+    mov r2, #0b100100000
+    mov r3, #1
     bl instructionCode
 
-    nanoSleep zeMeMama3
-    bl instructionCode
-    
-    mov r10, #0b10010
-    mov r12, #1
+    @Definindo interface
+    mov r2, #0b100101100
+    mov r3, #0
     bl instructionCode
 
-    mov r10, #0b100101000
-    mov r12, #1
+    @display on/off mode
+    mov r2, #0b100001100
+    mov r3, #0
+    bl instructionCode
+    
+    clearDisplay
+
+    @mode set
+    mov r2, #0b100000110
+    mov r3, #0
     bl instructionCode
 
-    mov r10, #0b100001000
-    mov r12, #1
-    bl instructionCode
-    
-    @clear display
-    mov r10, #0b100000001
-    mov r12, #1
-    bl instructionCode
-    
-    mov r10, #0b100000110
-    mov r12, #1
+.endm
+
+.macro enable
+    ldr r1, =pin_E
+    GPIOSet r1, #0
+    nanoSleep totalLoadingTimeRs
+    GPIOSet r1, #1
+    nanoSleep totalEnableTimeHigh
+    GPIOSet r1, #0
+    nanoSleep remainingEnableTime
+.endm
+
+.macro writeDegrees
+    mov r2, #0b011011111
+    mov r3, #0
     bl instructionCode
 .endm
+
+
+@r3 é para sinalizar se é pra incialização, usando somente 4 bits, ou normal - 8 bits (1 inicialização, 0 - transmissão normal)
+@ mov r2, 9 bits @r2 tem os bits de transmissao pro LCD
+@ r9 faz a máscara pra pegar bit a bit
+@ r6 é contador de 4 bits
+@ r11 é para registrar o endereço base dos pinos (pin_rs)
+instructionCode:
+    push {r1, r6, r9}
+    ldr r1, =pin_RS
+    and r9, r2, #0b100000000 @Mascara para pegar o mais significativo
+    lsr r9, #8 @Desloca para o bit menos significativo
+    eor r9, r9, #0b1
+    GPIOSet r1, r9 @Setagem do Pino RS
+    lsl r2, #1
+    mov r6, #1 @r10 é contador
+    add r1, #0x10 @Vai do pin_RS para o pin_D4 no data
+nibble:
+    and r9, r2, #0b100000000 @Mascara para pegar o mais significativo
+    lsr r9, #8 @Desloca para o bit menos significativo
+    GPIOSet r1, r9 @Setagem do Pino 
+    lsl r2, #1
+    add r6, #1 @incremento o contador
+    cmp r6, #4 @comparo com 4 o contador
+    add r1, #0x10 @Vai de um pino para o próximo
+    ble nibble
+    enable
+    cmp r3, #0
+    beq resetPinValue
+    pop {r1, r6, r9}
+    bx lr
+resetPinValue:
+    ldr r1, =pin_D7 @restaura o valor do endereço para o pin_D4
+    mov r6, #1
+    mov r3, #1
+    b nibble
+    
+    
+writeString:
+    push {lr}
+    clearDisplay
+    nanoSleep awaitInstructionHome
+loopWrite:
+    ldrb r2, [r5]
+    mov r3, #0
+    cmp r2, #10 @decimal do valor ascii de \n
+    beq writeSecondLine
+    cmp r2, #0 @decimal do valor ascii de \0
+    beq endWrite
+    
+    cmp r2, #42 @decimal do valor ascii de *
+    beq degreesWrite
+    
+    bl instructionCode
+    add r5, #0x1
+    b loopWrite
+writeSecondLine:
+    setSecondLine
+    add r5, #0x1
+    b loopWrite
+degreesWrite:
+    writeDegrees
+    add r5, #0x1
+    b loopWrite
+endWrite:
+    pop {pc}
+    bx lr
+
